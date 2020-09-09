200272025c5e3d0216e0ba759032feef970a7465
neharani289 <70572059+neharani289@users.noreply.github.com>
2020-09-10 01:22:40 +0530

    Update Assiggnment2.py

--- Assignment2/Assiggnment2.py
+++ Assignment2/Assiggnment2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import numpy as np
 m = ([[2,-3,3],[2,2,3],[3,-2,2]])
 print("Original matrix:")
@@ -12,8 +6,6 @@ result =  np.linalg.inv(m)
 print("Inverse of the said matrix:")
 print(result)
 
-
-# In[ ]:
 
 
 
